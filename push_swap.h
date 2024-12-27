73822f9ba96e6939e886dd0d82f2cfbd0e721083
el-035 <elisa.fittant@gmail.com>
2024-12-27 15:00:47 +0100

    d

---
+++ push_swap.h
@@ -0,0 +1,17 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+//all stuff to include
+# include "libft/libft.h"
+
+//all structs
+
+typedef struct s_stack
+{
+    int				data;
+    struct s_stack	*next;
+}					t_stack;
+
+//all prototypes
+
+#endif
